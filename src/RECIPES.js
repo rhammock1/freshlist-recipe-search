9aa78a4591967dd9ecb10fae6002b33bc6992e37
rhammock1 <rhamm1027@gmail.com>
2020-11-14 11:24:18 -0500

    Started working out dummy store

---
+++ src/RECIPES.js
@@ -0,0 +1,288 @@
+import cuid from 'cuid';
+
+const RECIPES = {
+  recipeType: [
+    {
+      id: cuid(),
+      type: 'Appetizer'
+    },
+    {
+      id: cuid(),
+      type: 'Entree'
+    },
+    {
+      id: cuid(),
+      type: 'Side'
+    },
+    {
+      id: cuid(),
+      type: 'Dessert'
+    }
+  ],
+  recipes: [
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    {
+      id: cuid(),
+      name: '',
+      recipeType: '',
+      ingredients: [
+        {
+          ingredient: '',
+          amount: 3,//a number
+          unit: lb,
+        }
+      ],
+      content: '',
+    },
+    
+    
+  ]
+}
+
+export default RECIPES;
