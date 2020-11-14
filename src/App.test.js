7c8f73dbd7bb21a52518a83ebf2e61b639397fbb
rhammock1 <rhamm1027@gmail.com>
2020-11-14 11:02:05 -0500

    Added first test in app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 describe('App component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+    <BrowserRouter>
+    
+    </BrowserRouter>
+      , div);
   })
 })
