602cac85e9ca298e6bfe7e2d102fa6d8f97353c7
rhammock1 <rhamm1027@gmail.com>
2020-11-17 13:47:37 -0500

    Added search form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import RecipeContext from './RecipeContext';
 import RECIPES from './RECIPES';
 import Header from './Components/Header/Header';
+import RecipeListMain from './Components/RecipeListMain/RecipeListMain';
+import { Route } from 'react-router-dom';
 
 class App extends React.Component {
   state = {
@@ -14,7 +16,10 @@ class App extends React.Component {
   render() {
     const value={RECIPES};
     return (
-      <Header />
+      <>
+        <Header />
+        <Route exact path="/" component={RecipeListMain} />
+      </>    
     )
   }
 }
---
+++ src/Components/RecipeSearchForm/RecipeSearchForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class RecipeSearchForm extends React.Component {
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log('Hey there boss')
+  }
+  render() {
+    return (
+//   The app doesn't really need the search option right now 
+//  It is just here so I don't have to do it later
+//  It won't really be neccesary until there are a lot more
+//  recipes in the dateabase
+//  Eventually this will all need to be refactored to use
+//  a real db and api and such
+
+      <form className='recipe-search' onSubmit={event => this.handleSubmit(event)}>
+        <fieldset>
+          <legend>Search for Recipes</legend>
+          <div className='form-group'>
+            <label htmlFor='search-bar'>Recipe: </label>
+            <input type='text' id='search-bar' name='search-bar' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='recipeType'>Recipe Type: </label>
+            <select id='recipeType' name='recipeType' >
+              <option value={null}>filter</option>
+              <option value='Appetizer'>Appetizer</option>
+              <option value='Entree'>Entree</option>
+              <option value='Side'>Side</option>
+              <option value='Sauce'>Sauce</option>
+              <option value='Beverage'>Beverage</option>
+              <option value='Dessert'>Dessert</option>
+            </select>
+          </div>
+          <button type='submit'>SEND IT!</button>
+        </fieldset>
+      </form>
+    )
+  }
+
+}
+
+export default RecipeSearchForm;
